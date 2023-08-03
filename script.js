0ea9c186935ed59db6258e9ce74869cb0b197cbb
Lucas Moreira <lucas_moreira@outlook.com>
2023-08-03 14:37:33 -0300

    limpeza dos arquivos old e criando portifolio do zero

--- script.js
+++
@@ -1,9 +0,0 @@
-function login() {
-    var user = document.getElementById('user').value;
-    var password = document.getElementById('password').value;
-
-        if (user == "admin" && password == "admin"){
-            location.href = "home.html"
-        }
-
-}
