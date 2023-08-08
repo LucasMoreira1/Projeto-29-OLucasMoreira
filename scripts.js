5e976f1a216d1a79456ace2b25947de9d10dc97c
Lucas Moreira <lucas_moreira@outlook.com>
2023-08-08 20:47:06 -0300

    inicio carrossel

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if(entry.isIntersecting){
-            entry.target.classList.replace('transition-effect0','transition-effect1');
+            entry.target.classList.replace('transition-effect-off','transition-effect-on');
         } else {
-            entry.target.classList.replace('transition-effect1','transition-effect0');
+            entry.target.classList.replace('transition-effect-on','transition-effect-off');
 
         }
     })
